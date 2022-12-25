72bb62de17ce5db7882008ba5d1dae01a87f86c7
Omar Farah <omaarfaarah2019@gmail.com>
2022-12-25 12:51:17 -0500

    fix axios issue

--- controllers/qurans.js
+++ controllers/qurans.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //get a random number
 export function getRandomVerse(req,res){
 
-    axios.get(`http://api.alquran.cloud/v1/ayah/${req.params.edition}`)
+    axios(`http://api.alquran.cloud/v1/ayah/${req.params.edition}`)
     .then(response=>{
       let data = response.data.data
         res.json(data)
@@ -13,14 +13,13 @@ export function getRandomVerse(req,res){
   })
 }
 export async function getAll(req,res){
-try {
-  await( axios.get(`http://api.alquran.cloud/v1/quran/ar.asad`))
+
+  axios(`http://api.alquran.cloud/v1/quran/ar.asad`)
   .then(response=>{
     let data = response.data.data
       res.json(data)
+    }). catch (error=> {
+      console.log(error.data)
+      res.status(500)
     })
-} catch (error) {
-  console.log(error)
-  res.status(500)
-}
 }
